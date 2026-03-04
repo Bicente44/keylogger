423b009b613ccc7e74fe6cf867f18f3d196e5353
Vincent Welbourne <vincent.vw04@gmail.com>
2026-03-04 16:55:40 -0500

    Feat: first commit create basic files and prepare
    This may not work for WSL as the /dev/input file is not nativealy
    available in WSL, this causes problems BUT i do so happen to have fedora
    already installed so may the project continue!
    
    Its a shame it may not work for WSL, once i get it working and good on
    actual linux ill attempt to make it work on WSL aswell
    
    (wsl pronounced as whistle -if you know you know)

---
+++ README.md
@@ -0,0 +1,2 @@
+# keylogger
+- This program is entirely made in C and is used to capture user keyboard input
---
+++ keylogger_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Simple shell of program (I have to commit as this may only work on linux not
+// WSL for the /dev/input file access)
+/* I wnat to be able to input int time as an argument for duration of logging */
+int main(int argc, char *argv[]) {
+
+  while (true) {
+  }
+
+  return 0;
+}
